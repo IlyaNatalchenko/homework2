585d3aaf1839dad98d239dac0ca307c0f0b09430
Ilian <ilia.natalchenko@gmail.com>
2022-07-27 14:52:27 +0300

    HomeWork 3

--- app/src/main/java/com/example/homework2/MainActivity.java
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.homework2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
+    private Button button2;
+
+    private int button01 = 0;
+    private int button02 = 0;
+
+    private TextView textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        textView1 = findViewById(R.id.textView1);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button){
+            this.button01 = 1;
+            textView1.setText(String.valueOf(button01));
+        }
+        else if (view.getId() == R.id.button2) {
+            this.button02 = 2;
+            textView1.setText(String.valueOf(button02));
+        }
     }
 }
